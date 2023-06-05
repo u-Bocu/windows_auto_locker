cf9a198dc1bff32d012ab744e00f63895c175c7c
Matthieu REMOND <matthieu.remond@sfr.fr>
2023-06-05 21:12:04 +0200

    add cmake config

---
+++ machine_learning.cpp
@@ -0,0 +1,5 @@
+//
+// Created by mremond on 05/06/2023.
+//
+
+#include "machine_learning.h"
---
+++ main.cpp
@@ -0,0 +1,9 @@
+//
+// Created by mremond on 05/06/2023.
+//
+#include "machine_learning.h"
+
+int main(void) {
+
+    return 0;
+}
